01d358c9eb734a804f946a2814bd8065c0ec21e5
dmitrenko <dmitrenko@lsp.team>
2021-04-22 12:24:04 +0300

    Initial commit

---
+++ api-grpc/src/main/java/com/example/apigrpc/ApiGrpcApplication.java
@@ -0,0 +1,13 @@
+package com.example.apigrpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGrpcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGrpcApplication.class, args);
+    }
+
+}
